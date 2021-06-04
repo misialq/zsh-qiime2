4f84df7581fefa1735bf92aab166ecedb100e30a
Michal Ziemski <mziemski@ethz.ch>
2021-06-04 17:34:03 +0200

    First instructions

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # zsh-qiime2
+
+Hi there, [QIIME 2](https://qiime2.org) users and developers! This is a small [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh)
+plugin providing a couple of (hopefully) useful functions and aliases that could make your work with QIIME2 slightly easier. 
+These include functions for automatic building of conda environments based on the latest QIIME2 release, functions for 
+testing conda builds (I'm looking at you, future plugin developers), a couple of aliases to simplify some verbose 
+commands and a bonus command prompt modifier - see below for details.
+
+### Installation
+   
+* Typically, you would install custom plugins under `$ZSH_CUSTOM/plugins`, so the easiest way is to just clone this 
+entire repository to that location:
+
+```shell
+git clone https://github.com/misialq/zsh-qiime2.git $ZSH_CUSTOM/plugins/zsh-qiime2
+```
+
+* After cloning, add the plugin name to the list of active plugins in your `.zshrc` file:
+
+```shell
+plugins=(... zsh-qiime2)
+```
+
+* Now, just source the .zshrc file or simply restart the terminal
+
+### Functionality
+
+#### Command prompt
+  
+This plugin lets you add a small modification to your command prompt that will display the QIIME 2 version
+installed in the currently activated conda environment. For now, it only supports the [Powerlevel10k](https://github.com/romkatv/powerlevel10k) 
+theme.
+
+To install the prompt, you need to add `qiime2` to the list of prompt segments in your theme configuration file (`~/.p10k.zsh`).
+It should look something like this (when added to the _right_ prompt):
+```shell
+# The list of segments shown on the right. Fill it with less important segments.
+# Right prompt on the last prompt line (where you are typing your commands) gets
+# automatically hidden when the input line reaches it. Right prompt above the
+# last prompt line gets hidden if it would overlap with left prompt.
+typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+# =========================[ Line #1 ]=========================
+qiime2
+status                  # exit code of the last command
+...
+```
+
+After sourcing the configuration file (or restarting your terminal), you should be able to see the current QIIME 2 version:
+* for prod releases: ![prod prompt](img/prompt_prod.png)
+* for dev releases: ![dev prompt](img/prompt_dev.png)
+
+#### Functions
+
+* `set_up_q2_full_env` - sets up a full QIIME 2 conda environment, using the latest QIIME 2 release (equivalent to 
+  what is described in the [documentation](https://docs.qiime2.org) under [Native installation](https://docs.qiime2.org/2021.4/install/native/#install-qiime-2-within-a-conda-environment) 
+  section)
+* `set_up_q2_base_env`- sets up a minimal QIIME 2 environment (qiime2, q2cli, q2-types), using the latest **dev** release
+    ```shell
+    set_up_q2_base_env <env_name>
+    ```
+    where `env_name` is the name for the new conda environment
+* `set_up_env`- adds a couple of useful dev packages to an existing environment (q2lint, flake8, coverage, wget, pytest-xdist)
+* `build_conda_pkg`- builds and tests a conda package for QIIME 2 plugins:
+    ```shell
+    build_conda_pkg <package_name> <qiime2_version> <os>
+    ```
+    where `qiime2_version` has format _2021.8_ and `os` is either _osx_ or _linux_
+
+#### Aliases
+
+|Alias|Command|
+|---|---|
+| q2 | qiime |
+| q2cit | qiime tools citations |
+| q2xt | qiime tools extract |
+| q2xp | qiime tools export |
+| q2i | qiime tools import |
+| q2it | qiime tools import --show-importable-types |
+| q2if | qiime tools import --show-importable-formats |
+| q2rc | qiime dev refresh-cache |
+| q2p | qiime tools peek |
+| q2v | qiime tools view |
+| q2val | qiime tools validate |
+| q2ins | qiime tools inspect-metadata |
+| piqr | pip install . && qiime dev refresh-cache |
+
+### Issues?
+
+Should you encounter any issues whatsoever, feel free to reach out - this is still a very early version
+of the plugin so there maybe a need of polishing this or that.
+
+**Happy QIIME'ing!**
---
+++ zsh-qiime2.plugin.zsh
@@ -0,0 +1,198 @@
+### Functions
+
+function current_q2_version() {
+  QIIME_INFO=$(qiime info) 2> /dev/null
+  if [[ $? == 0 ]]; then
+    QIIME2_VERSION=$(echo "$QIIME_INFO" | grep "QIIME 2 version" | sed "s/QIIME 2 version\://" | xargs)
+    QIIME2_RELEASE=$(echo "$QIIME_INFO" | grep "QIIME 2 release" | sed "s/QIIME 2 release\://" | xargs)
+  else
+    QIIME2_VERSION=""
+    QIIME2_RELEASE=""
+  fi
+
+  if [[ $QIIME2_VERSION = *dev* ]]; then
+    QIIME2_RELEASE="Q2:${QIIME2_RELEASE}(dev)"
+  elif [[ $QIIME2_VERSION != "" ]]; then
+    QIIME2_RELEASE="Q2:${QIIME2_RELEASE}"
+  else
+    QIIME2_RELEASE=""
+  fi
+
+  echo "$QIIME2_RELEASE"
+}
+
+function prompt_qiime2() {
+  current_q2_version > /dev/null
+  p10k segment -t "${QIIME2_RELEASE}" -f 121
+}
+
+function get_latest_q2_dev_version() {
+  latest_q2_tag=$(git ls-remote --tags --refs --sort 'v:refname' git@github.com:qiime2/qiime2.git | tail -1)
+  QIIME2_LATEST_DEV_VERSION=$(echo $latest_q2_tag | sed 's/.*tags\///' | xargs)
+  QIIME2_LATEST_DEV_RELEASE=$(echo $QIIME2_LATEST_DEV_VERSION | sed 's/\.[0-9]*\.dev.*$//' | xargs)
+}
+
+function get_latest_q2_prod_version() {
+  latest_q2_tags=($(git ls-remote --tags --refs --sort '-v:refname' git@github.com:qiime2/qiime2.git | sed 's/.*tags\///'))
+  for tag in $latest_q2_tags
+    do
+      if [[ $tag = *dev* ]]
+        then continue
+      else
+        QIIME2_LATEST_PROD_RELEASE=$(echo "$tag" | xargs)
+        break
+      fi
+  done
+}
+
+function set_up_env() {
+  CONDA_INFO=$(conda info)
+  CONDA_ENVS=$(echo "$CONDA_INFO" | grep "envs directories" | sed "s/envs directories \: //" | xargs)
+  CONDA_MAIN=$(echo $CONDA_ENVS | sed "s/\/envs//")
+  CONDA_CURRENT_ENV=$(echo "$CONDA_INFO" | grep "active environment" | sed "s/active environment \: //" | xargs)
+  CONDA_CURRENT_ENV_LOC=$(echo "$CONDA_INFO" | grep "active env location" | sed "s/active env location \: //" | xargs)
+
+  conda install -qy flake8 coverage wget pytest-xdist
+  python -m pip install -q https://github.com/qiime2/q2lint/archive/master.zip
+}
+
+function set_up_q2_base_env() {
+  get_latest_q2_dev_version
+  QIIME2_DEV_CHANNEL="https://packages.qiime2.org/qiime2/${QIIME2_LATEST_DEV_RELEASE}/staged"
+  ENV_NAME="$1"
+  COL="\033[0;32m"
+  NC="\033[0m"
+
+  if [[ "${ENV_NAME}" == "" ]]; then
+    ENV_NAME="q2-${QIIME2_LATEST_DEV_RELEASE}"
+  fi
+
+  echo "${COL}Creating conda environment (${ENV_NAME}) for QIIME 2 ${QIIME2_LATEST_DEV_VERSION}...${NC}"
+  conda deactivate
+  conda create -y -n "${ENV_NAME}" \
+   -c "${QIIME2_DEV_CHANNEL}" -c conda-forge -c bioconda -c defaults \
+   qiime2 q2cli q2-types click==7.1.2
+
+  conda activate "$ENV_NAME"
+  set_up_env
+
+  unset COL
+  unset NC
+  unset ENV_NAME
+}
+
+function set_up_q2_full_env() {
+  COL="\033[0;32m"
+  RED="\033[0;31m"
+  NC="\033[0m" # NoColor
+
+  if [[ "$OSTYPE" = "linux"* ]]
+    then OS_VER="linux"
+  elif [[ "$OSTYPE" = "darwin"* ]]
+    then OS_VER="osx"
+  else
+    echo "${RED}Detected OS version (${OSTYPE}) is not supported.${NC}"
+    echo "${RED}Aborting.${NC}"
+    return 1
+  fi
+
+  get_latest_q2_prod_version
+  echo "${COL}The latest available QIIME2 version is ${QIIME2_LATEST_PROD_RELEASE}.${NC}"
+  echo "${COL}Creating a full qiime2-$VER_SHORT environment...${NC}"
+
+  VER_SHORT=$(echo "$QIIME2_LATEST_PROD_RELEASE" | sed 's/\.[0-9]$//' | xargs)
+  DOWNLOAD_LINK="https://data.qiime2.org/distro/core/qiime2-${VER_SHORT}-py38-${OS_VER}-conda.yml"
+  ENV_FILE="env-spec-${VER_SHORT}.yaml"
+  curl -sL "$DOWNLOAD_LINK" -o "$ENV_FILE"
+  conda env create -n "qiime2-$VER_SHORT" --file "$ENV_FILE"
+  rm "$ENV_FILE"
+}
+
+function build_conda_pkg() {
+  # first arg should be name of the package to be built
+  # second arg should be Q2 version to test against (e.g. 2021.8)
+  # third arg should be OS version (osx or linux)
+  PKG_NAME="$1"
+  if test "PKG_NAME" = ""
+    then
+      echo "${RED}Name of the to-be-built package is required.${NC}"
+      echo "${RED}Build aborted.${NC}"
+      return 1
+  fi
+  PKG_NAME_UNDER="${PKG_NAME//-/_}"
+
+  Q2V="$2"
+  OSV="$3"
+  COL="\033[0;32m"
+  RED="\033[0;31m"
+  NC="\033[0m" # NoColor
+
+  if test "$Q2V" = ""
+    then Q2V="2021.8"
+  fi
+
+  if test "$OSV" =  ""
+    then OSV="osx"
+  fi
+
+  # check that required files exist
+  if ! test -f "conda_build_config.yaml" || ! test -f "meta.yaml"
+    then
+      echo "${RED}One of the required files (conda_build_config.yaml or meta.yaml) could not be found.${NC}"
+      echo "${RED}Build aborted.${NC}"
+      return 1
+  fi
+
+  conda deactivate
+  CONDA_ENVS=$(conda info | grep "envs directories" | sed "s/envs directories \: //" | xargs)
+  CONDA_MAIN=$(echo $CONDA_ENVS | sed "s/\/envs//")
+  echo "${COL}Detected conda path: ${CONDA_MAIN}${NC}"
+
+  echo "${COL}Preparing a new conda environment (qiime2-${Q2V}-buildtest)...${NC}"
+  conda create -y -n "qiime2-${Q2V}-buildtest" conda-build conda-verify wget
+  source "${CONDA_MAIN}/etc/profile.d/conda.sh"
+  conda activate "qiime2-${Q2V}-buildtest"
+  conda info
+
+  # build new package
+  echo "${COL}Starting build for QIIME2 ${Q2V} (${OSV})...${NC}"
+  conda build -c "https://packages.qiime2.org/qiime2/${Q2V}/staged" -c conda-forge -c bioconda -c defaults --override-channels --no-anaconda-upload --cache-dir conda_cache .
+
+  # test the build
+  echo "${COL}Testing the build...${NC}"
+  wget -O env.yml "https://raw.githubusercontent.com/qiime2/environment-files/master/${Q2V}/staging/qiime2-${Q2V}-py38-${OSV}-conda.yml"
+  conda env create -q -p "./testing-${Q2V}" --file env.yml
+  conda install -p "./testing-${Q2V}" -q -y -c "$CONDA_MAIN/conda-bld/${OSV}-64" -c conda-forge -c bioconda -c defaults --override-channels --strict-channel-priority "${PKG_NAME}"
+
+  conda activate "./testing-${Q2V}"
+  pytest --pyargs "${PKG_NAME_UNDER}"
+  conda deactivate
+
+  # clean up previous build tests
+  echo "${COL}Cleaning up...${NC}"
+  rm -rf "./testing-${Q2V}/*"
+  rm env.yml
+  conda build purge
+
+  conda deactivate
+  conda env remove -n "qiime2-${Q2V}-buildtest"
+
+  echo "${COL}All done!${NC}"
+
+}
+
+### Aliases
+
+alias q2='qiime'
+alias q2cit='qiime tools citations'
+alias q2xt='qiime tools extract'
+alias q2xp='qiime tools export'
+alias q2i='qiime tools import'
+alias q2it='qiime tools import --show-importable-types'
+alias q2if='qiime tools import --show-importable-formats'
+alias q2rc='qiime dev refresh-cache'
+alias q2p='qiime tools peek'
+alias q2v='qiime tools view'
+alias q2val='qiime tools validate'
+alias q2ins='qiime tools inspect-metadata'
+alias piqr='pip install . && qiime dev refresh-cache'
